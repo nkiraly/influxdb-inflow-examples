b5b2b2cb722b6a1666b501e23ec3af77dbe1eba3
Nicholas Kiraly <kiraly.nicholas@gmail.com>
2016-01-12 23:42:33 -0500

    expand reading data examples

--- src/main/java/org/influxdb/inflow/examples/ExamplesApp.java
+++ src/main/java/org/influxdb/inflow/examples/ExamplesApp.java
@@ -2,6 +2,8 @@ package org.influxdb.inflow.examples;
 
 import java.util.concurrent.TimeUnit;
 import org.influxdb.dto.Point;
+import org.influxdb.dto.Query;
+import org.influxdb.dto.QueryResult;
 import org.influxdb.inflow.Client;
 import org.influxdb.inflow.Database;
 import org.influxdb.inflow.InflowDatabaseException;
@@ -30,14 +32,57 @@ public class ExamplesApp {
     Client client = new Client("influxdb.local", 8086, "inflowexample", "inflow011");
     
     // You can also create a client from a URI describing the server
-    Client clientFromURI = Client.fromURI("http://inflowexample:inflow011@bludgeon:8086");
+    Client clientFromURI = Client.fromURI("http://inflowexample:inflow011@influxdb.local:8086");
     
     // if you use Database.fromURI(), it will be setup with the database name in the URI
-    Database databaseFromURI = Database.fromURI("http://inflowexample:inflow011@bludgeon:8086/inflow1");
+    Database databaseFromURI = Database.fromURI("http://inflowexample:inflow011@influxdb.local:8086/inflow1");
+  }
+  
+  public static void exampleReadData() throws InflowException {
+    String influxdbURI = "http://inflowexample:inflow011@influxdb.local:8086/inflow_test";
+    
+    // To fetch records from InfluxDB, you can:
+
+    // 1) Do a manual query directly on a database:
+    logger.info("Creating database object from URI " + influxdbURI);
+    Database database = Database.fromURI(influxdbURI);
+
+    // executing a raw query string yields a QueryResult object
+    String query1 = "SELECT * FROM test_metric LIMIT 5";
+    QueryResult result1 = database.query(query1);
+
+    // get the point values from the QueryResult as an array with the collapser getValuesAsStringArray
+    String[] values = result1.getValuesAsStringArray();
+    logger.info("got " + values.length + " values back:");
+    for( String value : values ) {
+      logger.info("value = " + value);
+    }
+
+    // 2) Query for data using the QueryBuilder object
+
+    // retrieve points value array with the query builder
+    String[] values2 = database.getQueryBuilder()
+            .select("cpucount")
+            .from("test_metric")
+            .limit(2)
+            .getQueryResult()
+            .getValuesAsStringArray();
+
+    // get the query command from the QueryBuilder
+    String query3 = database.getQueryBuilder()
+            .select("cpucount")
+            .from("test_metric")
+            .where("region = 'us-west'")
+            .getQueryCommand();
+    // Make sure that you enter single quotes when doing a where query on strings.
+    // Otherwise InfluxDB will return an empty result.
+
+    // You can get the last executed query from the client static method getLastQuery()
+    String lastQuery1 = Client.getLastQuery();
   }
   
   public static void exampleWriteData() throws InflowException, InflowDatabaseException {
-    String influxdbURI = "http://inflowexample:inflow011@bludgeon:8086";
+    String influxdbURI = "http://inflowexample:inflow011@influxdb.local:8086";
     
     logger.info("Creating client from URI " + influxdbURI);
     Client client = Client.fromURI(influxdbURI);
@@ -57,7 +102,7 @@ public class ExamplesApp {
             .tag("host", "server01")
             .tag("region", "us-west")
             .field("cpucount", 10)
-            .time(1435222310, TimeUnit.SECONDS) // 2015-06-25 08:51:50 GMT
+            .time(1452129125, TimeUnit.SECONDS) // 2016-01-07 01:12:05 GMT
             .build();
     // Note: It's possible to add multiple fields (see https://influxdb.com/docs/v0.9/concepts/key_concepts.html) when writing measurements to InfluxDB.
 
@@ -66,8 +111,27 @@ public class ExamplesApp {
             .field("value", 0.84)
             .build();
     // Note: No .time() - InfluxDB uses the current time as the default timestamp.
+    
+    // data points of test_metric measurement, which include optional tags and fields
+    Point point3 = Point
+            .measurement("test_metric")         // name of the measurement
+            .field("value", 0.64)               // the measurement value
+            .tag("host", "server01")            // optional tags
+            .tag("region", "us-west")
+            .field("cpucount", 10)              // optional additional fields
+            .time(1452659705, TimeUnit.SECONDS) // 2016-01-13 04:34:05 GMT
+            .build();
+    
+    Point point4 = Point
+            .measurement("test_metric")         // name of the measurement
+            .field("value", 0.84)               // the measurement value
+            .tag("host", "server01")            // optional tags
+            .tag("region", "us-west")
+            .field("cpucount", 10)              // optional additional fields
+            .time(1452659709, TimeUnit.SECONDS) // 2016-01-13 04:34:09 GMT
+            .build();
 
-    Point[] points = new Point[]{point1, point2};
+    Point[] points = new Point[]{point1, point2, point3, point4};
 
     logger.info("Writing " + points.length + " points to database " + database.getName());
     database.writePoints(points);
